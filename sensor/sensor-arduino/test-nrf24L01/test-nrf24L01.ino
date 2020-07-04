1c29cb6d258038caace3df049635ee78be9feb3e
Garth Webb <garth.webb@gmail.com>
2020-07-04 13:35:16 -0700

    Go back to default power value

--- sensor/sensor-arduino/test-nrf24L01/test-nrf24L01.ino
+++ sensor/sensor-arduino/test-nrf24L01/test-nrf24L01.ino
@@ -27,7 +27,7 @@ void setup() {
   radio.begin();
   // Set the PA Level low to prevent power supply related issues since this is a
   // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_LOW);
+  //radio.setPALevel(RF24_PA_LOW);
   
   // Open a writing and reading pipe on each radio, with opposite addresses
   if (radioNumber) {
